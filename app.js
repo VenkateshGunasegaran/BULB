80bf2393216c0b149dac6809dcc2878343e218df
VenkateshGunasegaran <139197390+VenkateshGunasegaran@users.noreply.github.com>
2023-07-14 14:53:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function change (){
+    var img = document.getElementById("bulb");
+    if (img.src.match("pngwing")){
+        img.src="https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png";
+    }
+    else {
+        img.src="https://w7.pngwing.com/pngs/670/121/png-transparent-incandescent-light-bulb-electric-light-bulb-angle-hand-lamp.png";
+    }
+}
